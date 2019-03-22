7bf39325f8fb98f46975ad5da48b9ef18625d530
ilya.romanov <ilya.romanov@travelline.ru>
2019-03-22 22:01:47 +0300

    Добавил каркас для обработки данных, возвращаемых источниками

---
+++ Core/ServiceDataManager/Adapters/OpenexchangeratesDataAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelLine.CurrencyRate.Core.ServiceDataManager.Adapters
+{
+    class OpenexchangeratesDataAdapter
+    {
+        public string connectionUrl;
+    }
+}
---
+++ Core/ServiceDataManager/DataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelLine.CurrencyRate.Core.ServiceDataManager
+{
+    class DataProvider
+    {
+        private List<ServiceData> servicesData;
+        public List<ServiceData> GetServicesData()
+        {
+            return servicesData;
+        }
+    }
+}
