22985058c411905eab3ddc19729a593fada6b6b0
ilya.romanov <ilya.romanov@travelline.ru>
2019-03-24 13:10:51 +0300

    Создал класс для взаимодействия с Frontend

---
+++ Core/Services/RateLoader/RateLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelLine.WebAppTemplate.Core.Data.EFRepository;
+using TravelLine.WebAppTemplate.Core.Data.Models.CurrencyRecord;
+
+namespace TravelLine.WebAppTemplate.Core.Services.RateLoader
+{
+    class RateLoader
+    {
+        private CurrencyRecordRepository currencyRepository = new CurrencyRecordRepository();
+        public void GetRates(DateTime date, string currencyCode)
+        {
+            List<CurrencyRecord> records = currencyRepository.GetItems(date, currencyCode);
+            foreach(CurrencyRecord record in records)
+            {
+                //if (record.Rate == null)
+            }
+        }
+    }
+}
