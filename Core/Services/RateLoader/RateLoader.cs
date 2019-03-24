e9944b054c9868cb95077df438526ffbbf9508cf
ilya.romanov <ilya.romanov@travelline.ru>
2019-03-24 15:29:13 +0300

    Добавил фильтрацию сервисов в классе RateLoader

--- Core/Services/RateLoader/RateLoader.cs
+++ Core/Services/RateLoader/RateLoader.cs
@@ -5,15 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using TravelLine.WebAppTemplate.Core.Data.EFRepository;
 using TravelLine.WebAppTemplate.Core.Data.Models.CurrencyRecord;
+using TravelLine.WebAppTemplate.Core.Data.Models.Service;
 
 namespace TravelLine.WebAppTemplate.Core.Services.RateLoader
 {
     class RateLoader
     {
         private CurrencyRecordRepository currencyRepository = new CurrencyRecordRepository();
+        private ServiceRepository serviceRepository = new ServiceRepository();
         public void GetRates(DateTime date, string currencyCode)
         {
+            List<Service> services = serviceRepository.GetServices();
             List<CurrencyRecord> records = currencyRepository.GetItems(date, currencyCode);
+            List<Service> servicesToCheck = services.Where(
+                service => !records.Exists(
+                    record => record.ServiceId == service.ServiceId
+                    )
+                ).ToList();
+
             foreach(CurrencyRecord record in records)
             {
                 //if (record.Rate == null)
