817295c9e400ef1e883c0c4b376700c9c7c02552
ilya.romanov <ilya.romanov@travelline.ru>
2019-03-28 19:44:06 +0300

    Добавил проект для тестов

---
+++ CurrencyRate/CurrencyRate.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyRate.Tests
+{
+    [TestClass]
+    public class ServicesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
