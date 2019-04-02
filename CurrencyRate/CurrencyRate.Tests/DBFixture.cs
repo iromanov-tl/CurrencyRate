3a2f74870f72098209d6a5b1075d8bf16e5ebe09
ilya.romanov <ilya.romanov@travelline.ru>
2019-04-02 19:21:09 +0300

    Создал каркас тестов репозиториев

---
+++ CurrencyRate/CurrencyRate.Tests/DBFixture.cs
@@ -0,0 +1,21 @@
+﻿using CurrencyRate.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CurrencyRate.Tests
+{
+    public class DBFixture
+    {
+        public DBFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection
+                .AddDbContext<CurrencyRateContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CurrencyRateContext-d09038ed-707a-43fd-85c5-ea671c289162;Trusted_Connection=True;MultipleActiveResultSets=true"),
+                    ServiceLifetime.Transient);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public ServiceProvider ServiceProvider { get; private set; }
+    }
+}
---
+++ CurrencyRate/CurrencyRate.Tests/DBTests.cs
@@ -0,0 +1,29 @@
+﻿using CurrencyRate.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CurrencyRate.Tests
+{
+    public class DBTests : IClassFixture<DBFixture>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public DBTests(DBFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            using (var context = _serviceProvider.GetService<CurrencyRateContext>())
+            {
+                Type type = context.Rate.GetType();
+            }
+        }
+
+    }
+}
