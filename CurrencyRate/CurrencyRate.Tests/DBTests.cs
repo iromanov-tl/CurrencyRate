2c1a4426bdcc0a229eb29bc77134310956c682b9
ilyarom <ilyarom5@gmail.com>
2019-04-02 23:28:53 +0300

    Доработал класс для тестирования базы данных

--- CurrencyRate/CurrencyRate.Tests/DBTests.cs
+++ CurrencyRate/CurrencyRate.Tests/DBTests.cs
@@ -1,42 +1,137 @@
 ﻿using CurrencyRate.Data.EFRepository;
 using CurrencyRate.Models;
+using CurrencyRate.Models.Rate;
+using CurrencyRate.Models.Service;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
 namespace CurrencyRate.Tests
 {
-    public class DBTests : IClassFixture<DBFixture>
+    public class DBTests
     {
         private ServiceProvider _serviceProvider;
         private readonly RateRepository _rateRepository;
         private readonly ServiceRepository _serviceRepository;
 
-        public DBTests(DBFixture fixture)
+        private readonly CurrencyRateContext _context;
+
+        private const string TestDate = "02.04.2019 0:00:00";
+
+        public DBTests()
         {
-            _serviceProvider = fixture.ServiceProvider;
-            _rateRepository = new RateRepository(_serviceProvider.GetService<CurrencyRateContext>());
-            _serviceRepository = new ServiceRepository(_serviceProvider.GetService<CurrencyRateContext>());
+            var options = new DbContextOptionsBuilder<CurrencyRateContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB")
+                .Options;
+            _context = new CurrencyRateContext(options);
+            _rateRepository = new RateRepository(_context);
+            _serviceRepository = new ServiceRepository(_context);
+            AddServicesData();
         }
+
+        private void AddServicesData()
+        {
+            List<Service> services = new List<Service> {
+                new Service {
+                    ServiceId = 1,
+                    Url = "service1.com"
+                },
+                new Service {
+                    ServiceId = 2,
+                    Url = "service2.com"
+                },
+                new Service {
+                    ServiceId = 3,
+                    Url = "service3.com"
+                }
+            };
+            _context.Service.AddRange(services);
+        }
+
+        private List<Rate> GetRatesTestData()
+        {
+            return new List<Rate> {
+                new Rate {
+                    ServiceId = 1,
+                    Code = "USD",
+                    Value = 65.5,
+                    Date = TestDate
+                },
+                new Rate {
+                    ServiceId = 1,
+                    Code = "EUR",
+                    Value = 70.5,
+                    Date = TestDate
+                },
+                new Rate {
+                    ServiceId = 1,
+                    Code = "RUB",
+                    Value = 1,
+                    Date = TestDate
+                },
+                new Rate {
+                    ServiceId = 2,
+                    Code = "USD",
+                    Value = 65.7,
+                    Date = TestDate
+                },
+                new Rate {
+                    ServiceId = 2,
+                    Code = "EUR",
+                    Value = 70.7,
+                    Date = TestDate
+                },
+                new Rate {
+                    ServiceId = 2,
+                    Code = "RUB",
+                    Value = 1,
+                    Date = TestDate
+                },
+                new Rate {
+                    ServiceId = 2,
+                    Code = "UAH",
+                    Value = 2.2,
+                    Date = TestDate
+                }
+            };
+        }
+
 
         [Fact]
         public void CanSaveRate()
         {
-            _rateRepository.
+            Exception expectedException = null;
+            List<Rate> testData = GetRatesTestData();
+            try {
+                _rateRepository.Save(testData);
+            }
+            catch(Exception exception)
+            {
+                expectedException = exception;
+            }
+            Assert.Null(expectedException);
         }
 
         [Fact]
-        public void CanGetValidItems()
+        public void CanGetValidItem()
         {
-            _rateRepository.
+            List<ReportingRate> rates = _rateRepository.GetItems(DateTime.Parse(TestDate).Date, "UAH");
+            Assert.Equal(1, rates.Count());
+            Assert.Equal("UAH", rates.Single().Code);
+            Assert.Equal("service2.com", rates.Single().ServiceUrl);
+            Assert.Equal(2.2, rates.Single().Value);
         }
 
         [Fact]
         public void CanGetServices()
         {
-            _serviceRepository.
+            var serviceCollection = new ServiceCollection();
+            List<Service> services = _serviceRepository.GetServices();
+            Assert.Equal(3, services.Count);
         }
 
     }
